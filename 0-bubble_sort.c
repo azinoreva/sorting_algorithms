04149ba4e2f49ef6af26315bb35d74504257cd8d
azinoreva <azinoreva@gmail.com>
2024-03-08 12:13:52 -0800

    Betty correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,24 +3,33 @@
 #include "sort.h"
 
 /*
- * Bubble Sort implementation with print_array function.
+ * bubble_sort: implementation with print_array function.
  *
  * Parameters:
  *   - array: Pointer to the array to be sorted.
  *   - size: Number of elements in the array.
  */
-void bubble_sort(int *array, size_t size){
-    unsigned long int i, j;
 
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size - (i + 1); j++) {         
-            if (array[j] > array[j + 1]) {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+void bubble_sort(int *array, size_t size)
+	{
+		unsigned long int i, j;
 
-                print_array(array, size);
-            }
-        }
-    }
-}
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - (i + 1); j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					int temp = array[j];
+
+					array[j] = array[j + 1];
+
+					array[j + 1] = temp;
+
+					print_array(array, size);
+
+				}
+			}
+		}
+	}
+

d94dd69fd72efc66ff75ab3e1ab9f30cc69bc982
azinoreva <azinoreva@gmail.com>
2024-03-09 13:50:04 -0800

    Corrected Errors

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 
 #endif /* SORT_H */

616038b5fbeafe00b9b2d0d23dd237cd049ea682
azinoreva <azinoreva@gmail.com>
2024-03-09 13:38:59 -0800

    Passsed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,47 @@
 #include "sort.h"
-/*
-*insert sort implementation
-* Parameters
-* - list a double pointer to a structures list
-*/
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
 
-void insertion_sort_list(listint_t **list) {
-    listint_t *sorted = NULL;
-    listint_t *current = *list;
-
-    while (current != NULL) {
-        listint_t *next = current->next;
-
-        if (sorted == NULL || sorted->n >= current->n) {
-            current->next = sorted;
-            sorted = current;
-        } else {
-            listint_t *temp = sorted;
-            listint_t *prev = NULL;
-
-            while (temp != NULL && temp->n < current->n) {
-                prev = temp;
-                temp = temp->next;
-            }
-
-            if (prev != NULL) {
-                prev->next = current;
-                current->next = temp;
-            } else {
-                current->next = sorted;
-                sorted = current;
-            }
-        }
-
-        print_list(*list);
-        current = next;
-    }
-
-    *list = sorted;
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
+/**
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
+
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,34 @@
 #include "sort.h"
-
-/*
- * selection_sort - An algorithm that implements selection
-Parameters: array  and size of array
-*/
-
+/**
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @size: size of the array
+ * @array: list with numbers
+ */
 void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, temp, flag = 0;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
 	{
-		size_t min;
-		size_t j;
-		size_t i;
-
-		for (i = 0; i < size - 1; i++)
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
 			{
-				min = i;		
-				for (j = i + 1; j < size; j++)
-					{
-						if (array[j] < array[min])
-							{
-								min = j;
-							}
-					}
-				if (min != i)
-					{
-						int temp = array[i];
-
-						array[i] = array[min];
-						
-						array[min] = temp;
-					}
-				print_array(array, size);
+				tmp = index;
+				flag += 1;
 			}
+		}
+		temp = array[i];
+		array[i] = array[tmp];
+		array[tmp] = temp;
+		if (flag != 0)
+			print_array(array, size);
 	}
+}
 
